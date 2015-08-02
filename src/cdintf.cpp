3cb2a37a8d3538767aae48498cd88b6eaed0146f
twinaphex <libretro@gmail.com>
2015-08-02 02:10:29 +0200

    Remove HAVE_LIB_CDIO support

--- src/cdintf.cpp
+++ src/cdintf.cpp
@@ -21,13 +21,9 @@
 // (Actually, this is defined in the Makefile to prevent having to edit
 //  things too damn much. Jury is still out whether or not to make this
 //  change permanent.)
-//#define HAVE_LIB_CDIO
 
 #include "cdintf.h"								// Every OS has to implement these
 
-#ifdef HAVE_LIB_CDIO
-#include <cdio/cdio.h>							// Now using OS agnostic CD access routines!
-#endif
 #include "log.h"
 
 
@@ -55,37 +51,16 @@ static void TestCDIO(void)
 // *** OK, here's where we're going to attempt to put the platform agnostic CD interface ***
 //
 
-#ifdef HAVE_LIB_CDIO
-static CdIo_t * cdioPtr = NULL;
-#endif
-
 bool CDIntfInit(void)
 {
-#ifdef HAVE_LIB_CDIO
-	cdioPtr = cdio_open(NULL, DRIVER_DEVICE);
-
-	if (cdioPtr == NULL)
-	{
-#endif
 		WriteLog("CDINTF: No suitable CD-ROM driver found.\n");
 		return false;
-#ifdef HAVE_LIB_CDIO
-	}
-
-	WriteLog("CDINTF: Successfully opened CD-ROM interface.\n");
-
-	return true;
-#endif
 }
 
 void CDIntfDone(void)
 {
 	WriteLog("CDINTF: Shutting down CD-ROM subsystem.\n");
 
-#ifdef HAVE_LIB_CDIO
-	if (cdioPtr)
-		cdio_destroy(cdioPtr);
-#endif
 }
 
 bool CDIntfReadBlock(uint32_t sector, uint8_t * buffer)
@@ -124,14 +99,7 @@ const uint8_t * CDIntfGetDriveName(uint32_t driveNum)
 #warning "!!! FIX !!! CDIntfGetDriveName driveNum is currently ignored!"
 	// driveNum is currently ignored... !!! FIX !!!
 
-#ifdef HAVE_LIB_CDIO
-	uint8_t * driveName = (uint8_t *)cdio_get_default_device(cdioPtr);
-	WriteLog("CDINTF: The drive name for the current driver is %s.\n", driveName);
-
-	return driveName;
-#else
 	return (uint8_t *)"NONE";
-#endif
 }
 
 uint8_t CDIntfGetSessionInfo(uint32_t session, uint32_t offset)
