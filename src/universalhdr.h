dc40164f4ec6af8cc1ec9d887a7100fe7f621eda
twinaphex <libretro@gmail.com>
2015-08-28 14:11:44 +0200

    Convert universalhdr.cpp to C

--- src/universalhdr.h
+++ src/universalhdr.h
@@ -5,6 +5,14 @@
 #ifndef __UNIVERSALHDR_H__
 #define __UNIVERSALHDR_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern unsigned char universalCartHeader[];
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif	// __UNIVERSALHDR_H__
