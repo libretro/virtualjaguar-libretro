abecba4da39a4c983cf5fadd7a257c5d24e7d706
twinaphex <libretro@gmail.com>
2021-10-08 05:54:48 +0200

    Backport some changes from Joe

--- src/joystick.c
+++ src/joystick.c
@@ -22,7 +22,7 @@
 static uint8_t joystick_ram[4];
 uint8_t joypad0Buttons[21];
 uint8_t joypad1Buttons[21];
-bool audioEnabled = false;
+bool audioEnabled     = false;
 bool joysticksEnabled = false;
 
 void JoystickInit(void)
@@ -52,10 +52,10 @@ void JoystickDone(void)
 uint16_t JoystickReadWord(uint32_t offset)
 {
 	/* E, D, B, 7 */
-	uint8_t joypad0Offset[16] = {
+	const uint8_t joypad0Offset[16] = {
 		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x0C, 0xFF, 0xFF, 0xFF, 0x08, 0xFF, 0x04, 0x00, 0xFF
 	};
-	uint8_t joypad1Offset[16] = {
+	const uint8_t joypad1Offset[16] = {
 		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x04, 0xFF, 0x08, 0x0C, 0xFF
 	};
 
@@ -63,7 +63,6 @@ uint16_t JoystickReadWord(uint32_t offset)
 
 	if (offset == 0)
 	{
-		unsigned i;
 		uint8_t offset0, offset1;
 		uint16_t data = 0xFFFF;
 
@@ -77,6 +76,7 @@ uint16_t JoystickReadWord(uint32_t offset)
 
 		if (offset0 != 0xFF)
 		{
+			unsigned i;
 			uint16_t mask[4] = { 0xFEFF, 0xFDFF, 0xFBFF, 0xF7FF };
 			uint16_t msk2[4] = { 0xFFFF, 0xFFFD, 0xFFFB, 0xFFF7 };
 
@@ -88,6 +88,7 @@ uint16_t JoystickReadWord(uint32_t offset)
 
 		if (offset1 != 0xFF)
 		{
+			unsigned i;
 			uint16_t mask[4] = { 0xEFFF, 0xDFFF, 0xBFFF, 0x7FFF };
 			uint16_t msk2[4] = { 0xFF7F, 0xFFBF, 0xFFDF, 0xFFEF };
 
@@ -105,6 +106,7 @@ uint16_t JoystickReadWord(uint32_t offset)
 		// Hardware ID returns NTSC/PAL identification bit here
 		// N.B.: On real H/W, bit 7 is *always* zero...!
 		uint16_t data = 0xFF6F | (vjs.hardwareTypeNTSC ? 0x10 : 0x00);
+		const int8_t mask[4][2] = { { BUTTON_A, BUTTON_PAUSE }, { BUTTON_B, 0xFF }, { BUTTON_C, 0xFF }, { BUTTON_OPTION, 0xFF } };
 
 		if (!joysticksEnabled)
 			return data;
@@ -116,18 +118,14 @@ uint16_t JoystickReadWord(uint32_t offset)
 
 		if (offset0 != 0xFF)
 		{
-			int8_t mask[4][2] = { { BUTTON_A, BUTTON_PAUSE }, { BUTTON_B, 0xFF }, { BUTTON_C, 0xFF }, { BUTTON_OPTION, 0xFF } };
 			data &= (joypad0Buttons[mask[offset0][0]] ? 0xFFFD : 0xFFFF);
-
 			if (mask[offset0][1] != 0xFF)
 				data &= (joypad0Buttons[mask[offset0][1]] ? 0xFFFE : 0xFFFF);
 		}
 
 		if (offset1 != 0xFF)
 		{
-			int8_t mask[4][2] = { { BUTTON_A, BUTTON_PAUSE }, { BUTTON_B, 0xFF }, { BUTTON_C, 0xFF }, { BUTTON_OPTION, 0xFF } };
 			data &= (joypad1Buttons[mask[offset1][0]] ? 0xFFF7 : 0xFFFF);
-
 			if (mask[offset1][1] != 0xFF)
 				data &= (joypad1Buttons[mask[offset1][1]] ? 0xFFFB : 0xFFFF);
 		}
